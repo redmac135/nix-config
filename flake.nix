820485e466f05785aa00774928f08bdd28d75c4d
Ethan Zhao <ethan.yzhao@outlook.com>
2026-07-25 21:17:59 +0800

    fix: add home-manager to flake.nix

--- flake.nix
+++ flake.nix
@@ -3,14 +3,15 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
 
-	home-manager.url = "github:nix-community/home-manager";
-	home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = {
     self,
     nixpkgs,
     nixos-wsl,
+    home-manager,
     ...
   }: {
     nixosConfigurations = {
@@ -18,14 +19,14 @@
         system = "aarch64-linux";
         modules = [
           nixos-wsl.nixosModules.default
-		  ./configuration.nix
+          ./configuration.nix
 
-		  home-manager.nixosModules.home-manager
-		  {
-		    home-manager.useGlobalPkgs = true;
-			home-manager.useUserPackages = true;
-			home-manager.users.ezhao = import ./home.nix;
-		  }
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.ezhao = import ./home.nix;
+          }
         ];
       };
     };
