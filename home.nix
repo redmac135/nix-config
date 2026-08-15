b5b932895c02df2c4f0972f6097e82da02f9d5ba
Ethan Zhao <ethan.yzhao@outlook.com>
2026-08-15 03:17:28 +0000

    add pi (llm-agents) + package pi-web web UI

--- home.nix
+++ home.nix
@@ -63,6 +63,7 @@
     firstmate.lavish-axi
     firstmate.tasks-axi
     firstmate.quota-axi
+    firstmate.pi-web
   ];
 
   # ---------------------------------------------------------------------------
