aeab1e7272657f4288ba34a0ae1a65191a1bdafe
Ethan Zhao <ethan.yzhao@outlook.com>
2026-08-23 20:47:45 +0000

    nixos: enable Tailscale service

--- configuration.nix
+++ configuration.nix
@@ -19,6 +19,8 @@
   # Enable Docker daemon at system level
   virtualisation.docker.enable = true;
 
+  services.tailscale.enable = true;
+
   # mirror Arch wsl fix: https://gitlab.archlinux.org/archlinux/archlinux-wsl/-/work_items/16
   systemd.services."getty@tty1".enable = false;
 
