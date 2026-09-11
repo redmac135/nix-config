21ab909fd387818f6ee9bbdbce44b1b2d0dbd4a5
Ethan Zhao <ethan.yzhao@outlook.com>
2026-09-11 12:16:55 -0400

    apply cache in nix config (#18)
    if we apply the cache substituter in settings.nix, it only applies after
    successful switches, if we do so in nixConfig, it applies before the
    switch

--- flake.nix
+++ flake.nix
@@ -1,4 +1,11 @@
 {
+  nixConfig = {
+    extra-substituters = ["https://cache.numtide.com"];
+    extra-trusted-public-keys = [
+      "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
+    ];
+  };
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
