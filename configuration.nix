8258de051befe9dcfdda34e6a6e8392c0c3bd25f
Ethan Zhao <ethan.yzhao@outlook.com>
2026-09-15 14:28:16 -0400

    add onhand pubkey (#24)

--- configuration.nix
+++ configuration.nix
@@ -18,6 +18,8 @@
     ];
     shell = pkgs.zsh;
     openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGwaOLmlmVgJwwxG/ColvYAL/D0ZehXqnFiPgV/DEwzw ezhao@nixos
+"
     ];
   };
 
